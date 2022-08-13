ef69607cc8921cafdf21ed4b6e9770538b93c3dd
OkayAlex <108826506+OkayAlex@users.noreply.github.com>
2022-08-13 15:33:43 +0200

    Add files via upload

--- Random1.py
+++ Random1.py
@@ -1,24 +1,6 @@
+from dataclasses import dataclass
 import sqlite3
 import random
-
-
-
-conn = sqlite3.connect('game.db') # Создание базы данных
-
-cur = conn.cursor() # Создаем объект cur. Он позволяет делать SQL-запросы к базе. Используем переменную cur для хранения объекта
-
-cur.execute("""CREATE TABLE IF NOT EXISTS Enemies(
-   Enemy_name TEXT PRIMARY KEY, 
-   Enemy_hp INT,
-   Enemy_atk INT);  
-""") # Создаем таблицу в бд. 
-conn.commit()
-
-
-all_enemies = [('ork', '2', '1'), ('dragon', '4', '2'), ('boss', '6', '3')]
-cur.executemany("INSERT OR REPLACE INTO Enemies VALUES(?, ?, ?);", all_enemies)
-conn.commit()
-
 
 class Player: 
     """Инициализация игрока"""
@@ -43,25 +25,25 @@ class Player:
             self.player_exp = 0
             self.player_Mexp = 10
             self.player_heal = 3
-            print("Вы умерли. Начните заново")
+            
 
     def upgrade(self):
-        while self.player_sp > 0:
+        if self.player_sp > 0:
             print(f"Доступны очки навыков. Кол-во очков {self.player_sp}")
             print("Нажмите 1, чтобы повысить HP на 5")
             print("Нажмите 2, чтобы повысить атаку на 1")
             print("Нажмите 3, чтобы повысить исцеление на 2")
             action = input("> ")
-        if action == "1":
-            self.player_hp += 5
-            self.player_sp -= 1
-            self.player_Mhp += 5
-        if action == "2":
-            self.player_atk += 2
-            self.player_sp -= 1
-        if action == "3":
-            self.player_heal += 1
-            self.player_sp -= 1
+            if action == "1":
+                self.player_hp += 5
+                self.player_sp -= 1
+                self.player_Mhp += 5
+            elif action == "2":
+                self.player_atk += 2
+                self.player_sp -= 1
+            elif action == "3":
+                self.player_heal += 1
+                self.player_sp -= 1
 
     def check_stats(self):
         print("Ваши текущие характеристики:")
@@ -72,7 +54,7 @@ class Player:
         print(f"Уровень: {self.player_lvl}")
         input("Enter чтобы продолжить")
 
-Hero = Player(10, 10, 2, 0, 0, 0, 100, 3)
+
 
 
 class Enemy:
@@ -82,91 +64,154 @@ class Enemy:
         self.enemy_atk = enemy_atk
         self.enemy_exp = enemy_exp
 
-    def battle(self):
+@dataclass    
+class EnemyTemplate:
+    enemy_name: str
+    enemy_hp: int
+    enemy_atk: int
+    enemy_exp: int
 
-        print("Вы встречаете существо " + str(self.enemy_name))
-        print("Его здоровье = " + str(self.enemy_hp))
-        print("Его атака = " + str(self.enemy_atk))
 
-        while self.enemy_hp > 0:
+# Сделать класс битва.
+
+class Battle():
+
+    def __init__(self, hero, enemy):
+        self.hero = hero
+        self.enemy = enemy
+
+    def attack(self):
+        self.enemy.enemy_hp -= self.hero.player_atk
+        print(f"Вы нанесли урон: {self.hero.player_atk}")
+        self.hero.player_hp -= self.enemy.enemy_atk
+        print(f"Вам нанесли урон: {self.enemy.enemy_atk}")
+        if self.hero.player_hp < 0:
+            print("Вы проиграли...")
+            self.hero.stats_basic()
+            
+
+    def heal(self):
+        self.hero.player_hp -= self.enemy.enemy_atk
+        print(f"Вам нанесли урон: {self.enemy.enemy_atk}")
+        self.hero.player_hp += self.hero.player_heal
+        if self.hero.player_hp <= 0:
+            print("Вы проиграли")
+            self.hero.stats_basic()
+            
+        if self.hero.player_hp >= 0: 
+            print(f"Вы восстановили здоровье  (+{self.hero.player_heal})")
+        if self.hero.player_hp > self.hero.player_Mhp:
+            self.hero.player_hp = self.hero.player_Mhp
+        print(f"Текущее здоровье: {self.hero.player_hp}")
+
+    def retreat(self):
+        retreat = random.randint(1, 3)
+        if retreat == 2:
+            print("Вам удается убежать!")
+            random_event()
+            
+        else:
+            print("Вам не удается сбежать")
+            print("Вы проиграли...")
+            hero.stats_basic()
+            
+
+    def lvl_up(self):
+        if self.hero.player_exp >= self.hero.player_Mexp:
+            self.hero.player_exp = 0
+            self.hero.player_Mexp *= 5
+            self.hero.player_lvl += 1
+            self.hero.player_sp += 1
+            self.hero.upgrade()     
+            print(f"Текущий опыт: {self.hero.player_exp} ")
+
+    def reward(self):
+        if self.enemy.enemy_hp <= 0:
+            print(f"Вы победили, получите +{self.enemy.enemy_exp} ед. опыта ")
+            self.enemy.enemy_hp = 0
+            self.hero.player_exp += self.enemy.enemy_exp
+            if self.hero.player_exp >= self.hero.player_Mexp:
+                self.lvl_up()
+
+    def start_battle(self):
+
+        print("Вы встречаете существо " + str(self.enemy.enemy_name))
+        print("Его здоровье = " + str(self.enemy.enemy_hp))
+        print("Его атака = " + str(self.enemy.enemy_atk))
+
+        while self.enemy.enemy_hp > 0:
             print("Нажмите 1, чтобы атаковать")
-            print(f"Нажмите 2, чтобы вылечиться (+{Hero.player_heal})")
+            print(f"Нажмите 2, чтобы вылечиться (+{self.hero.player_heal})")
             print("Нажмите 3, чтобы убежать")
             player_choice = input("> ")
 
             if player_choice == "1":
-                self.enemy_hp -= Hero.player_atk
-                print(f"Вы нанесли урон: {Hero.player_atk}")
-                Hero.player_hp -= self.enemy_atk
-                print(f"Вам нанесли урон: {self.enemy_atk}")
-                if Hero.player_hp < 0:
-                    print("Вы проиграли...")
-                    Hero.stats_basic()
-                    random_event()
-
-            if player_choice == "2":
-                Hero.player_hp -= self.enemy_atk
-                print(f"Вам нанесли урон: {self.enemy_atk}")
-                Hero.player_hp += Hero.player_heal
-                if Hero.player_hp <= 0:
-                    print("Вы проиграли")
-                    Hero.stats_basic()
-                    random_event()
-                if Hero.player_hp >= 0: 
-                    print(f"Вы восстановили здоровье  (+{Hero.player_heal})")
-                if Hero.player_hp > Hero.player_Mhp:
-                    Hero.player_hp = Hero.player_Mhp
-                print(f"Текущее здоровье: {Hero.player_hp}")
-            
-            if player_choice == "3":
-                retreat = random.randint(1, 3)
-                if retreat == 2:
-                    print("Вам удается убежать!")
-                    random_event()
-                else:
-                    print("Вам не удается сбежать")
-                    print("Вы проиграли...")
-                    Hero.stats_basic()
-                    random_event()
-         
-            if Hero.player_exp >= Hero.player_Mexp:
-                Hero.player_exp = 0
-                Hero.player_Mexp *= 5
-                Hero.player_lvl += 1
-                Hero.upgrade()     
-                print(f"Текущий опыт: {Hero.player_exp} ")
-            
-            if self.enemy_hp <= 0:
-                print(f"Вы победили, получите +{self.enemy_exp} ед. опыта ")
+                self.attack()
+        
+            elif player_choice == "2":
+                self.heal()
                 
-ork = Enemy("Орк", 2, 1, 2)
-dragon = Enemy("Дракон", 4, 2, 3)
-boss = Enemy("Босс", 6, 2, 5)
+            elif player_choice == "3":
+                self.retreat()
+                
+        self.lvl_up()
+        self.reward()
 
 
-print("Вы входите в магический куб рандома. Приготовтесь к испытаниям.")
 
-def random_event():
+def random_event(hero, enemy_list):
+
     while True:
         print("Что делаем?")
         print("Нажми 1, чтобы пойти сражаться")
         print("Нажми 2, чтобы проверить свои характеристики")
         answer = input("> ")
         if answer == "1":
-            random_battle()
-        if answer == "2":
-            Hero.check_stats()
+            random_battle(hero, enemy_list)
+        elif answer == "2":
+            hero.check_stats()
+        else:
+            break
         
 
-def random_battle():
-    enemies = [1,2,3]
-    battle = random.choice(enemies)
-    if battle == 1:
-        ork.battle()
-    if battle == 2:
-        dragon.battle()
-    if battle == 3:
-        boss.battle()
+def random_battle(hero, enemy_list):
+    
+    enemy_template = random.choice(enemy_list)
+    enemy = Enemy(enemy_template.enemy_name, enemy_template.enemy_hp, enemy_template.enemy_atk, enemy_template.enemy_exp)
+    battle = Battle(hero, enemy)
+    battle.start_battle()
+    
     
 
-random_event()
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect('game.db') # Создание базы данных
+
+    cur = conn.cursor() # Создаем объект cur. Он позволяет делать SQL-запросы к базе. Используем переменную cur для хранения объекта
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS Enemies(
+    Enemy_name TEXT PRIMARY KEY, 
+    Enemy_hp INT,
+    Enemy_atk INT);  
+    """) # Создаем таблицу в бд. 
+    conn.commit()
+
+    all_enemies = [('ork', '2', '1'), ('dragon', '4', '2'), ('boss', '6', '3')] # Сделать так, чтобы бд читала уже существующие объекты класса.
+    cur.executemany("INSERT OR REPLACE INTO Enemies VALUES(?, ?, ?);", all_enemies)
+    conn.commit()
+
+    
+
+    ork = EnemyTemplate("Орк", 2, 1, 2)
+    dragon = EnemyTemplate("Дракон", 4, 2, 3)
+    boss = EnemyTemplate("Босс", 6, 2, 5)
+
+    enemy_list = [ork, dragon, boss]
+
+    hero = Player(10, 10, 2, 0, 0, 0, 10, 3)
+    print("Вы входите в магический куб рандома. Приготовтесь к испытаниям.")
+    random_event(hero, enemy_list)
+    
+    
+
